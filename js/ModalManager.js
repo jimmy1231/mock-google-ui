f43ffd071684425ca3449bcaca55b428d7bf8192
Jimmy Li <dfjimmy.li@gmail.com>
2018-10-10 20:07:25 -0400

    Clicking modal in background closes it

--- js/ModalManager.js
+++ js/ModalManager.js
@@ -3,26 +3,31 @@ class ModalManager {
 		this.modals = {
 			gmail: {
 				modal: document.getElementById("gmail-modal"), 
+				background: document.getElementById("gmail-background"),
 				container: document.getElementById("gmail-container"),
 				close: document.getElementById("gmail-close")
 			},
 			images: {
 				modal: document.getElementById("images-modal"),
+				background: document.getElementById("images-background"),
 				container: document.getElementById("images-container"),
 				close: document.getElementById("images-close")
 			},
 			cloud: {
 				modal: document.getElementById("cloud-modal"),
+				background: document.getElementById("cloud-background"),
 				container: document.getElementById("cloud-container"),
 				close: document.getElementById("cloud-close")
 			},
 			notifications: {
 				modal: document.getElementById("notifications-modal"),
+				background: document.getElementById("notifications-background"),
 				container: document.getElementById("notifications-container"),
 				close: document.getElementById("notifications-close")
 			},
 			profile: {
 				modal: document.getElementById("profile-modal"),
+				background: document.getElementById("profile-background"),
 				container: document.getElementById("profile-container"),
 				close: document.getElementById("profile-close")
 			}
@@ -30,11 +35,18 @@ class ModalManager {
 
 		this.registerModals(
 			Object.keys(this.modals).map(key => this.modals[key].container), 
-			this.openHandler);
+			this.openHandler
+		);
+
+		this.registerModals(
+			Object.keys(this.modals).map(key => this.modals[key].background), 
+			this.closeHandler
+		);
 
 		this.registerModals(
 			Object.keys(this.modals).map(key => this.modals[key].close), 
-			this.closeHandler);
+			this.closeHandler
+		);
 	}
 
 	registerModals(modals, handler) {
